5e6b1c95e8571b74e4d328dca81e769b34dc2daf
jobel <jobel@DESKTOP-D6GVMTO>
2022-09-15 16:26:11 +0200

    Projektdateien hinzufügen.

---
+++ MvcMovie_4.0/Data/MvcMovie_4_0Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie_4._0.Models;
+
+namespace MvcMovie_4._0.Data
+{
+    public class MvcMovie_4_0Context : DbContext
+    {
+        public MvcMovie_4_0Context (DbContextOptions<MvcMovie_4_0Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie_4._0.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ MvcMovie_4.0/Models/MovieGenreViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MvcMovie_4._0.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie>? Movies { get; set; }
+        public SelectList? Genres { get; set; }
+        public string? MovieGenre { get; set; }
+        public string? SearchString { get; set; }
+    }
+}
